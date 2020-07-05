09aecf1d08ad768a2ca041f1da61a2c9554e370a
ofan45 <ofan45@gmail.com>
2020-07-05 18:23:50 +0700

    Quiz ofan45

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,11 +12,12 @@
   <title>SB Admin 2 - Blank</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="{{asset('/sbadmin2/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="{{asset('/sbadmin2/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -41,9 +42,14 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="/">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/artikel">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Artikel</span></a>
       </li>
 
       <!-- Divider -->
@@ -332,8 +338,7 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
-
+            @yield('content')
         </div>
         <!-- /.container-fluid -->
 
@@ -380,20 +385,22 @@
     </div>
   </div>
 
+
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('/sbadmin2/vendor/jquery/jquery.min.js')}}"></script>
+  
+  <script src="{{asset('/sbadmin2/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="{{asset('/sbadmin2/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="{{asset('/sbadmin2/js/sb-admin-2.min.js')}}"></script>
 
   <!-- script tambahan sweet alert, bukan dari bawaan sb-admin-2 -->
   @stack('scripts')
 
-  <script src="js/swal.min.js"></script>
+  <script src="{{asset('/sbadmin2/js/swal.min.js')}}"></script>
 
 </body>
 
